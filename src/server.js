5f26ee21f87db8194d747e1af2d72c71c8b1f686
lucas <lucasriibeiro23@gmail.com>
2023-11-13 16:15:03 -0300

    finaliza

--- src/server.js
+++ src/server.js
@@ -6,4 +6,5 @@ const PORT = 3002;
 
 app.listen(PORT, async () => {
   console.log(`API Trybecash está sendo executada na porta ${PORT}`);
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
 });
